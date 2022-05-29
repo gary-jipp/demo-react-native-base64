5a62694197bcf4a4879868717c6fce5bc859a527
Gary J <github@accu.ca>
2022-05-29 11:44:07 -0700

    Add Header Test

--- __tests__/Header-test.js
+++ __tests__/Header-test.js
@@ -1,15 +1,16 @@
 import 'react-native';
 import React from 'react';
-import App from 'App';
+import Header from 'Header';
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-describe('App Tests', () => {
+describe('Header Tests', () => {
 
   it('Renders without crashing', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<Header title="Application Header"/>).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
 });
